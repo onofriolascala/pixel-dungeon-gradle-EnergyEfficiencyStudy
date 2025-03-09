750d21b810b1a966bc1de11e41ad4a2818ef44dc
onofrio <on.lascala@studenti.unina.it>
2025-03-09 17:44:08 +0100

    test efficienza energetica

--- core/src/main/java/com/watabou/pixeldungeon/Badges.java
+++ core/src/main/java/com/watabou/pixeldungeon/Badges.java
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 package com.watabou.pixeldungeon;
-//ma
+//maa
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
