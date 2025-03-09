c2a51708cc6725061b9f5965b91f6205d2a07ecd
onofrio <on.lascala@studenti.unina.it>
2025-03-09 17:08:32 +0100

    test per verificare test energetico

--- core/src/main/java/com/watabou/pixeldungeon/Badges.java
+++ core/src/main/java/com/watabou/pixeldungeon/Badges.java
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 package com.watabou.pixeldungeon;
-
+//m
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
