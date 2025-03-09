bcfaa5fb933a32281156b1bd4a3a00343deba8f1
onofrio <on.lascala@studenti.unina.it>
2025-03-09 17:32:46 +0100

    test

--- core/src/main/java/com/watabou/pixeldungeon/Assets.java
+++ core/src/main/java/com/watabou/pixeldungeon/Assets.java
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 package com.watabou.pixeldungeon;
-//aa
+//aaa
 public class Assets {
 
 	public static final String ARCS_BG		= "arcs1.png";
